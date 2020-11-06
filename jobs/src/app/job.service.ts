f705a41a4ce9334eb3df3b9a1e283b8a1f9ac8bb
marin bosnjak <bosnjakmarin@outlook.com>
2020-11-06 13:11:08 +0100

    Fixed styling error in job service

--- jobs/src/app/job.service.ts
+++ jobs/src/app/job.service.ts
@@ -1,26 +1,29 @@
-import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
-import { Job } from './job.model';
+import {Injectable} from '@angular/core';
+import {Subject} from 'rxjs';
+import {Job} from './job.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JobService {
-  headerTitle = new Subject<string>()
-  setloader = new Subject<boolean>()
-  errorHappened = new Subject<boolean>()
+  headerTitle = new Subject<string>();
+  setloader = new Subject<boolean>();
+  errorHappened = new Subject<boolean>();
 
-  private loadedJobs : Job[] 
-  
-  constructor() { }
+  private loadedJobs: Job[];
 
-  getJob(idd : string) : Job {
+  constructor() {
+  }
+
+  getJob(idd: string): Job {
     return this.loadedJobs.find(job => job.id === idd);
   }
-  setJobs( jobs : Job[]){
-    this.loadedJobs = jobs
+
+  setJobs(jobs: Job[]) {
+    this.loadedJobs = jobs;
   }
-  getJobs(){
-    return this.loadedJobs
+
+  getJobs() {
+    return this.loadedJobs;
   }
 }
