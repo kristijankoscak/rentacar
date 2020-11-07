8371014d87a4793cbc4132e539296a3a74600fa2
Kristijan Koščak <kristijan.koscak16@gmail.com>
2020-11-07 13:55:18 +0100

    Solved styling errors in posts component file.

--- task-news-reader/src/app/posts/posts.component.ts
+++ task-news-reader/src/app/posts/posts.component.ts
@@ -4,22 +4,22 @@ import { PostService } from './post.service';
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css','../app.component.css']
+  styleUrls: ['./posts.component.css', '../app.component.css']
 })
 export class PostsComponent implements OnInit {
 
   isLoading = true;
-  error:string;
+  error: string;
 
-  constructor(private postService:PostService) { }
+  constructor(private postService: PostService) { }
 
   ngOnInit(): void {
     this.postService.postsLoading.subscribe(state => {
       this.isLoading = state;
-    })
+    });
     this.postService.occurredError.subscribe(errorMessage => {
       this.error = errorMessage;
-    })
+    });
   }
 
 }
