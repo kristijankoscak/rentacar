673da898f491904ccebe7f0950b0a31aa26ba7e5
Kristijan Koščak <kristijan.koscak16@gmail.com>
2020-11-10 16:23:11 +0100

    Fixed styling errors found with ts lint in posts.model.ts file.

--- task-news-reader/src/app/posts/post.model.ts
+++ task-news-reader/src/app/posts/post.model.ts
@@ -1,9 +1,9 @@
 export class Post {
-  public imagePath:string;
-  public title:string;
-  public description:string;
+  public imagePath: string;
+  public title: string;
+  public description: string;
 
-  constructor(imagePath:string,title:string,description:string){
+  constructor(imagePath: string, title: string, description: string){
     this.imagePath = imagePath;
     this.title = title;
     this.description = description;
