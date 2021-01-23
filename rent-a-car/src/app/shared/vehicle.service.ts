6b480838905f1d3983c91893ac6a6e46ea2cfa79
Kristijan Koščak <kristijan.koscak16@gmail.com>
2021-01-24 00:48:44 +0100

    WIP - vehicle detail and vehicle edit flow...

--- rent-a-car/src/app/shared/vehicle.service.ts
+++ rent-a-car/src/app/shared/vehicle.service.ts
@@ -1,18 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Vehicle } from '../vehicle/vehicle.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehicleService {
-  vehicles : Vehicle[] = [];
+  vehicles: Vehicle[] = [];
   filteredVehicles: Vehicle[]=[];
   specificVehicle: Vehicle;
   vehiclesChanged = new Subject<Vehicle[]>();
-  vehicleIsPicked = new Subject<Vehicle>();
 
-  constructor() { }
+  constructor(
+    private http: HttpClient,
+    private router:Router
+  ) { }
   setVehicles(vehicles: Vehicle[]): void {
     this.vehicles = vehicles;
     this.vehiclesChanged.next(this.vehicles.slice());
@@ -32,16 +38,105 @@ export class VehicleService {
   getVehiclesByCarRental(carRentalName: string): Vehicle[]{
     return this.vehicles.filter(vehicle => vehicle.carRental.name === carRentalName);
   }
-
-  async fetchVehicleByID(id:number): Promise<void>{
-    await this.setVehicleByID(id);
-    console.log(this.specificVehicle)
-    this.vehicleIsPicked.next(this.specificVehicle);
-  }
-  setVehicleByID(id:number): void{
-    this.specificVehicle = this.vehicles.find((vehicle)=>{return vehicle.id === id});
-  }
   getVehicle(id: number): Vehicle{
     return this.vehicles.find(vehicle => vehicle.id === id);
+  }
+  getVehiclesByRentalId(id: number): Vehicle[]{
+    const rentalVehicles = this.vehicles.filter(vehicle => vehicle.carRental.id === id);
+    return rentalVehicles;
+  }
+  updateVehicle(vehicle:any): void{
+    this.updateVehicleLocal(vehicle);
+    this.vehiclesChanged.next(this.vehicles);
+    this.updateVehicleInBase(vehicle).subscribe();
+    this.router.navigate(['/vehicle']);
+  }
+  updateVehicleLocal(vehicle:any): void{
+    this.vehicles.find((veh,index) => {
+      if(veh.id === vehicle.id){
+        if(!vehicle.images){
+           this.vehicles[index] = {
+             ...vehicle,
+             images: this.vehicles[index].images
+           }
+        }
+        else{
+          this.vehicles[index] = {
+            ...vehicle,
+          };
+        }
+      }
+    })
+  }
+  updateVehicleInBase(vehicle:any): Observable<any>{
+    return this.http
+    .put<string>(
+      environment.apiUrl + '/vehicles/'+vehicle.id,
+      vehicle
+    )
+    .pipe(
+      tap(
+        (response: string) => {
+          console.log(response);
+        },
+        (errorResponse: string)=> {
+          console.log(errorResponse)
+        }
+      )
+    );
+  }
+  addNewVehicle(vehicle:any): Observable<any>{
+    return this.http
+    .post<any>(
+      environment.apiUrl + '/vehicles/',
+      vehicle
+    )
+    .pipe(
+      tap(
+        (response: string) => {
+          console.log(response);
+        },
+        (errorResponse: string)=> {
+          console.log(errorResponse)
+        }
+      )
+    );
+  }
+
+  deleteVehicle(id:number) {
+    this.deleteVehicleLocal(id);
+    this.deleteVehicleFromBase(id).subscribe();
+  }
+  deleteVehicleLocal(id:number): void{
+    this.vehicles = this.vehicles.filter(vehicle => vehicle.id !== id);
+    this.vehiclesChanged.next(this.vehicles);
+  }
+  deleteVehicleFromBase(id:number): Observable<any>{
+    return this.http
+    .delete<any>(
+      environment.apiUrl + '/vehicles/'+id
+    )
+    .pipe(
+      tap(
+        (response: string) => {
+          console.log(response);
+        },
+        (errorResponse: string)=> {
+          console.log(errorResponse)
+        }
+      )
+    );
+  }
+
+  fetchVehicles(): Observable<Vehicle[]>{
+    return this.http
+    .get<any>(
+      environment.apiUrl + '/vehicles'
+    )
+    .pipe(
+      tap((vehicles: Vehicle[]) => {
+        this.setVehicles(vehicles);
+      })
+    );
   }
 }
